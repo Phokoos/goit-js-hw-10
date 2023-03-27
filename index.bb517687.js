51ea4a8d5accdc6d47981ef2937f73af3614273b
Phokoos <83214849+Phokoos@users.noreply.github.com>
2023-03-27 17:26:06 +0000

    Deploying to gh-pages from @ Phokoos/goit-js-hw-10@059de6009c477c82f287fc2bdcb2b94e499c0e09 🚀

--- index.bb517687.js
+++
@@ -1,2 +0,0 @@
-var e,a;e="Ukraine".toLowerCase(),(a=e,fetch(`https://restcountries.com/v3.1/name/${a}?fields=name,capital,population,flags,languages`).then((e=>{if(!e.ok)throw new Error(e.status);return e.json()}))).then((e=>{console.log(e[0].name.official)}));
-//# sourceMappingURL=index.bb517687.js.map
--- index.f300a5dc.js
+++
@@ -1,2 +0,0 @@
-!function(){"use strict";var n,t,o;n="Ukraine".toLowerCase(),(t=n,o="https://restcountries.com/v3.1/name/".concat(t,"?fields=name,capital,population,flags,languages"),fetch("".concat(o)).then((function(n){if(!n.ok)throw new Error(n.status);return n.json()}))).then((function(n){console.log(n[0].name.official)}))}();
-//# sourceMappingURL=index.f300a5dc.js.map
