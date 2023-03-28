afa69e06492513db7999032affe08bfcc3e72a25
Phokoos <hulkovskij@gmail.com>
2023-03-28 21:38:43 +0300

    Finished work at project

--- src/index.js
+++ src/index.js
@@ -20,47 +20,15 @@ const DEBOUNCE_DELAY = 300;
 // part with styles
 
 inputFieldEl.style.width = "400px";
-// inputFieldEl.style.margin = "20px";
+
 bodyEl.style.display = "flex";
 bodyEl.style.alignItems = "center";
 bodyEl.style.flexWrap = "wrap";
 bodyEl.style.flexDirection = "column";
 
-
-
-
-function imitationApi(country) {
-	const countryGlobalTest = [
-		{
-			name: "Ukraine",
-			src: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Flag_of_Ukraine.svg/800px-Flag_of_Ukraine.svg.png",
-			capital: "Kyiv",
-			population: "46000000",
-			languages: "Ukrainian"
-		},
-		// {
-		// 	name: "Great Britian",
-		// 	src: "https://cdn.britannica.com/25/4825-050-977D8C5E/Flag-United-Kingdom.jpg",
-		// 	capital: "London",
-		// 	population: "55000000",
-		// 	languages: "English"
-		// },
-		// {
-		// 	name: "Poland",
-		// 	src: "https://upload.wikimedia.org/wikipedia/en/thumb/1/12/Flag_of_Poland.svg/800px-Flag_of_Poland.svg.png",
-		// 	capital: "Wrazlav",
-		// 	population: "36000000",
-		// 	languages: "Polish"
-		// },
-	];	
-	return countryGlobalTest;
-};
-
-
-
 // add event listener with debounce
 
-inputFieldEl.addEventListener("input", debounce(addHtmlCode, DEBOUNCE_DELAY))
+inputFieldEl.addEventListener("input", debounce(addHtmlCode, DEBOUNCE_DELAY));
 
 // if user cleanes field return err and clean HTML
 
@@ -80,46 +48,54 @@ function addHtmlCode() {
 function workWithResultFromApi(countryName) {
 	countryListEl.innerHTML = "";
 	countryDivEl.innerHTML = "";
-	const inputLength = countryName.length;
 
-	const countryArrayFromApi = imitationApi();
-	const countryArrayLength = countryArrayFromApi.length;
+	checkResult(countryName);
+}
+
+const checkResult = (countryName) => {
+	classApi = new fetchCountries;
+	classApi.fetchCountries(countryName)
+		.then((data) => {
+			workWithUserInterface(data);
+			return data;
+		})
+		.catch((error) => {
+			return console.log("Error: " + error);
+		})
+}
+
+function workWithUserInterface(data) {
+	const countryArrayLength = data.length;
+
+	if (data === 404) {
+		return Notiflix.Notify.failure('Oops, there is no country with that name');
+	}
 
 	if (countryArrayLength > 10) {
 		return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
 	}
-	if (2 <= countryArrayLength && countryArrayLength <= 10) {
-		const markup = countryArrayFromApi.map(({src , name}) => `<li style="display:flex;align-items:center;gap:10px;">
-        <img src=${src} alt="" width="50px" />
-        <p><b>${name}</b></p>
-      </li>`).join("");
 
+	if (2 <= countryArrayLength && countryArrayLength <= 10) {
+		const markup = data.map(({flags , name}) => `<li style="display:flex;align-items:center;gap:10px;">
+        <img src=${flags.svg} alt="" width="50px" />
+        <p><b>${name.official}</b></p>
+      </li>`).join("");
 		return countryListEl.insertAdjacentHTML("beforeend", markup);
 	}
+
 	if (countryArrayLength === 1) {
-		const markup = countryArrayFromApi.map(({ name, src, capital, population, languages }) =>
-			`<div style="display: flex; align-items: center; gap: 10px; height:60px">
-       	 <img src=${src} alt="" width="50px" />
-      	  <p style="font-size: 40px"><b>${name}</b></p>
+		const markup = data.map(({ name, flags, capital, population, languages }) => 
+				`<div style="display: flex; align-items: center; gap: 10px; height:60px">
+       	 <img src=${flags.svg} alt="" width="50px" />
+      	  <p style="font-size: 40px"><b>${name.official}</b></p>
     	 	 </div>
      		 <ul style="padding: 0;list-style: none;">
         		<li><b>Capital:</b> ${capital}</li>
        		 <li><b>Population:</b> ${population}</li>
-       		 <li><b>Languages:</b> ${languages}</li>
+       		 <li><b>Languages:</b> ${Object.values(languages)}</li>
       	</ul>`
+		
 		).join("");
 		return countryDivEl.insertAdjacentHTML("beforeend", markup);
 	}
 }
-
-let countryName = "Ukraine";
-
-const checkResult = (countryName) => {
-	fetchCountries(countryName)
-		.then((data) => {
-		
-		console.log(data[0].name.official);
-	})
-}
-
-// checkResult(countryName.toLowerCase());
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,17 +1,23 @@
 'use strict';
 
 //  find and return dates from API
+export class fetchCountries {
+	fetchCountries(countryName) { 
+	  const BASE_URL = `https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`;
 
-export function fetchCountries(countryName) {
-	// Make export using name!
-	// file fields which i want
-	const BASE_URL = `https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`;
+		return fetch(`${BASE_URL}`)
+			.then(
+				res => {
+					if (res.status === 404) {
+						// return res.json();
+						return res.status;
+					}
 
-	return fetch(`${BASE_URL}`)
-		.then(res => {
-			if (!res.ok) {
-				throw new Error(res.status);
-			}
-			return res.json();
-	})
-}
+					if (!res.ok) {
+						throw new Error(res.status);
+					}
+
+					return res.json();
+				});
+	};
+};
